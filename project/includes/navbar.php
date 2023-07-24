5da293ebef3c4564c8e1abafde34daccb0ab13fa
HAW ENG HONG <heh00261147@gmail.com>
2023-07-24 23:49:39 +0800

    update my menu to drop down list type for order

--- project/includes/navbar.php
+++ project/includes/navbar.php
@@ -9,18 +9,41 @@
                 <li class="nav-item">
                     <a class="nav-link" href="index.php">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="product_read.php">Product List</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="productDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Product
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="productDropdown">
+                        <a class="dropdown-item" href="product_create.php">Create Product</a>
+                        <a class="dropdown-item" href="product_read.php">Product List</a>
+                    </div>
                 </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="customer_read.php">Customer List</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="customerDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Customer
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="customerDropdown">
+                        <a class="dropdown-item" href="customer_create.php">Create Customer</a>
+                        <a class="dropdown-item" href="customer_read.php">Customer List</a>
+                    </div>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="category_read.php">Category List</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="categoryDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Category
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="categoryDropdown">
+                        <a class="dropdown-item" href="category_create.php">Create Category</a>
+                        <a class="dropdown-item" href="category_read.php">Category List</a>
+                    </div>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="order_read.php">Order List</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="orderDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Order
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="orderDropdown">
+                        <a class="dropdown-item" href="create_order.php">Create Order</a>
+                        <a class="dropdown-item" href="order_read.php">Order List</a>
+                    </div>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="contact.php">Contact Us</a>
