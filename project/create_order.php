7ac33af02cad8bb21a220a1bcd7571fa68c5a750
HAW ENG HONG <heh00261147@gmail.com>
2023-07-24 23:25:54 +0800

    done that remain product

--- project/create_order.php
+++ project/create_order.php
@@ -1,156 +1,182 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Create Order</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container">
-        <?php
-        include 'includes/navbar.php';
-        ?>
+        <?php include 'includes/navbar.php'; ?>
         <div class="page-header">
-            <h1>Create New Order</h1>
+            <h1>Create Order</h1>
         </div>
-
         <?php
-        date_default_timezone_set('asia/Kuala_Lumpur');
-        // include database connection
         include 'config/database.php';
+        $customer_query = "SELECT id, first_name FROM customers";
+        $customer_stmt = $con->prepare($customer_query);
+        $customer_stmt->execute();
+        $customers = $customer_stmt->fetchAll(PDO::FETCH_ASSOC);
         $product_query = "SELECT id, name FROM products";
         $product_stmt = $con->prepare($product_query);
         $product_stmt->execute();
         $products = $product_stmt->fetchAll(PDO::FETCH_ASSOC);
-
+        $selected_product_count = 1;
+        $error = array();
+        $product_id = '';
         if ($_POST) {
-
+            $product_id = $_POST["product"];
+            $quantity = $_POST["quantity"];
+            $customer_id = $_POST['customer'];
+            $selected_product_count = count($_POST['product']);
             try {
-                $summary_query = "INSERT INTO order_summary SET customer_id=:customer, order_date=:order_date";
-                $customer = $_POST['customer'];
-                $order_date = date('Y-m-d H:i:s'); // get the current date and time
-                $summary_stmt = $con->prepare($summary_query);
-                $summary_stmt->bindParam(':customer', $customer);
-                $summary_stmt->bindParam(':order_date', $order_date);
-                $summary_stmt->execute();
-                // order details
-                $details_query = "INSERT INTO order_details SET order_id=:order_id, customer_id=:customer_id, product_id=:product_id, quantity=:quantity";
-                $order_id = $con->lastInsertId();
-                $details_stmt = $con->prepare($details_query);
-                $product_id = $_POST['product'];
-                $quantity = $_POST['quantity'];
-                for ($i = 0; $i < count($product_id); $i++) {
-                    // array
-                    $details_stmt->bindParam(':order_id', $order_id);
-                    $details_stmt->bindParam(':customer_id', $customer);
-                    $details_stmt->bindParam(':product_id', $product_id[$i]);
-                    $details_stmt->bindParam(':quantity', $quantity[$i]);
-                    $details_stmt->execute();
+                if ($customer_id == "") {
+                    $error[] = "Please choose your name.";
                 }
-                echo "<div class='alert alert-success'>Order successfully placed.</div>";
+                if (isset($selected_product_count)) {
+                    for ($i = 0; $i < $selected_product_count; $i++) {
+                        if ($product_id[$i] == "") {
+                            $error[] = " Please choose product " . $i + 1 . ".";
+                        }
+                        if ($quantity[$i] == 0 || empty($quantity[$i])) {
+                            $error[] = "Quantity cannot be zero or empty.";
+                        } else if ($quantity[$i] < 0) {
+                            $error[] = "Quantity cannot be negative.";
+                        } else if (!is_numeric($quantity[$i])) {
+                            $error[] = "Quantity must be numberic.";
+                        }
+                    }
+                }
+                if (!empty($error)) {
+                    echo "<div class='alert alert-danger role='alert'>";
+                    foreach ($error as $error_message) {
+                        echo $error_message . "<br>";
+                    }
+                    echo "</div>";
+                } else {
+                    $customer_id = $_POST['customer'];
+                    date_default_timezone_set('Asia/Kuala_Lumpur');
+                    $order_date = date('Y-m-d H:i:s');
+                    $order_summary_query = "INSERT INTO order_summary SET customer_id=:customer_id, order_date=:order_date";
+                    $order_summary_stmt = $con->prepare($order_summary_query);
+                    $order_summary_stmt->bindParam(":customer_id", $customer_id);
+                    $order_summary_stmt->bindParam(":order_date", $order_date);
+                    $order_summary_stmt->execute();
+                    $order_id = $con->lastInsertId(); //Get the order_id from last inserted row.
+                    for ($i = 0; $i < $selected_product_count; $i++) {
+                        $order_details_query = "INSERT INTO order_details SET order_id=:order_id, product_id=:product_id, quantity=:quantity";
+                        $order_details_stmt = $con->prepare($order_details_query);
+                        $order_details_stmt->bindParam(":order_id", $order_id);
+                        $order_details_stmt->bindParam(":product_id", $product_id[$i]);
+                        $order_details_stmt->bindParam(":quantity", $quantity[$i]);
+                        $order_details_stmt->execute();
+                    }
+                    echo "<div class='alert alert-success' role='alert'>Order Placed Successfully.</div>";
+                }
             } catch (PDOException $exception) {
-                echo "<div class='alert alert-danger'>Unable to place order.</div>";
+                echo '<div class="alert alert-danger role=alert">' . $exception->getMessage() . '</div>';
             }
         }
         ?>
+        <div>
+            <form action="" method="post">
+                <select name="customer" id="customer" class="form-select w-50 my-4">
+                    <option value="">Choose your name</option>
+                    <?php
+                    for ($a = 0; $a < count($customers); $a++) {
+                        $customer_selected = isset($_POST["customer"]) && $customers[$a]['id'] == $_POST["customer"] ? "selected" : "";
+                        echo "<option value='{$customers[$a]['id']}' $customer_selected>{$customers[$a]['first_name']}</option>";
+                    }
+                    ?>
+                </select>
+                <table class="table table-hover table-responsive table-bordered" id="row_del">
+                    <tr>
+                        <th>NO.</th>
+                        <th>Product</th>
+                        <th>Quantity</th>
+                        <th>Actions</th>
+                    </tr>
+                    <?php
+                    $product_loop = (!empty($error)) ? $selected_product_count : 1;
+                    for ($a = 0; $a < $product_loop; $a++) {
+                    ?>
+                        <tr class="pRow">
+                            <td class="col-1">
+                                <?php echo $a + 1; ?>
+                            </td>
+                            <td>
+                                <select name="product[]" id="product" class="form-select" value>
+                                    <option value="">Choose a Product</option>
+                                    <?php
+                                    for ($i = 0; $i < count($products); $i++) {
+                                        $product_selected = isset($_POST["product"]) && $products[$i]['id'] == $_POST["product"][$a] ? "selected" : "";
+                                        echo "<option value='{$products[$i]['id']}' $product_selected>{$products[$i]['name']}</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </td>
+                            <td>
+                                <input type="number" class="form-control" name="quantity[]" id="quantity" value="<?php echo isset($_POST['quantity']) ? $_POST['quantity'][$a] : 1; ?>">
 
-        <form action="" method="POST">
-            <span>Select Customer</span>
-            <select class="form-select mb-3" name="customer">
-                <option value="" selected disabled>Choose a customer</option>
-                <?php
-                // Fetch customers from the database
-                $query = "SELECT id, first_name FROM customers";
-                $stmt = $con->prepare($query);
-                $stmt->execute();
-                $customers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                            </td>
+                            <td>
+                                <input href='#' onclick='deleteRow(this)' class='btn d-flex justify-content-center btn-danger mt-1' readonly value="Delete" />
+                            </td>
+                        </tr>
+                    <?php }
+                    ?>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <input type="button" value="Add More Product" class="btn btn-success add_one" />
+                        </td>
+                        <td></td>
+                    </tr>
+                </table>
 
-                // Generate select options
-                foreach ($customers as $customer) {
-                    echo "<option value='{$customer['id']}'>{$customer['first_name']}</option>";
-                } ?>
-            </select>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Place Order</button>
+                    <a href="" class="btn btn-danger">Back to Read Order summary</a>
+                </div>
+            </form>
+            <script>
+                document.addEventListener('click', function(event) {
+                    if (event.target.matches('.add_one')) {
+                        var rows = document.getElementsByClassName('pRow');
+                        // Get the last row in the table
+                        var lastRow = rows[rows.length - 1];
+                        // Clone the last row
+                        var clone = lastRow.cloneNode(true);
+                        // Insert the clone after the last row
+                        lastRow.insertAdjacentElement('afterend', clone);
 
-            <table class='table table-hover table-responsive table-bordered' id="row_del">
-                <tr>
-                    <td class="text-center">#</td>
-                    <td class="text-center">Product</td>
-                    <td class="text-center">Quantity</td>
-                    <td class="text-center">Action</td>
-                </tr>
-                <tr class="pRow">
-                    <td class="text-center">1</td>
-                    <td class="d-flex">
-                        <select class="form-select" name="product[]">
-                            <option value="" selected disabled>Choose a product</option>
-                            <?php
-                            // Generate select options
-                            foreach ($products as $product) {
-                                echo "<option value='{$product['id']}'>{$product['name']}</option>";
-                            }
-                            ?>
-                        </select>
-                    </td>
-                    <td><input class="form-control" type="number" name="quantity[]"></td>
-                    <td><input href='#' onclick='deleteRow(this)' class='btn d-flex justify-content-center btn-danger mt-1' value="Delete" /></td>
-                </tr>
-                <tr>
-                    <td>
+                        // Loop through the rows
+                        for (var i = 0; i < rows.length; i++) {
+                            // Set the inner HTML of the first cell to the current loop iteration number
+                            rows[i].cells[0].innerHTML = i + 1;
+                        }
+                    }
+                }, false);
 
-                    </td>
-                    <td colspan="4">
-                        <input type="button" value="Add More Product" class="btn btn-success add_one" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
+                function deleteRow(r) {
+                    var total = document.querySelectorAll('.pRow').length;
+                    if (total > 1) {
+                        var i = r.parentNode.parentNode.rowIndex;
+                        document.getElementById("row_del").deleteRow(i);
 
-                    </td>
-                    <td colspan="4"><input type='submit' value='Place Order' class='btn btn-primary' /></td>
-                </tr>
-            </table>
-        </form>
-        <script>
-            document.addEventListener('click', function(event) {
-                if (event.target.matches('.add_one')) {
-                    var rows = document.getElementsByClassName('pRow');
-                    // Get the last row in the table
-                    var lastRow = rows[rows.length - 1];
-                    // Clone the last row
-                    var clone = lastRow.cloneNode(true);
-                    // Insert the clone after the last row
-                    lastRow.insertAdjacentElement('afterend', clone);
-
-                    // Loop through the rows
-                    for (var i = 0; i < rows.length; i++) {
-                        // Set the inner HTML of the first cell to the current loop iteration number
-                        rows[i].cells[0].innerHTML = i + 1;
+                        var rows = document.getElementsByClassName('pRow');
+                        for (var i = 0; i < rows.length; i++) {
+                            // Set the inner HTML of the first cell to the current loop iteration number
+                            rows[i].cells[0].innerHTML = i + 1;
+                        }
+                    } else {
+                        alert("You need order at least one item.");
                     }
                 }
-            }, false);
-
-            function deleteRow(r) {
-                var total = document.querySelectorAll('.pRow').length;
-                if (total > 1) {
-                    var i = r.parentNode.parentNode.rowIndex;
-                    document.getElementById("row_del").deleteRow(i);
-
-                    var rows = document.getElementsByClassName('pRow');
-                    for (var i = 0; i < rows.length; i++) {
-                        // Set the inner HTML of the first cell to the current loop iteration number
-                        rows[i].cells[0].innerHTML = i + 1;
-                    }
-                } else {
-                    alert("You need to order at least one item.");
-                }
-            }
-        </script>
+            </script>
+        </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKr  diJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
-
 </html>
