e73fdeebc08c83a0da17e227c94f10015e499812
HAW ENG HONG <heh00261147@gmail.com>
2023-08-07 14:37:08 +0800

    show customer name and order date time

--- project/order_read_one.php
+++ project/order_read_one.php
@@ -1,3 +1,5 @@
+</html>
+
 <!DOCTYPE HTML>
 <html>
 
@@ -11,10 +13,26 @@
         <?php include 'includes/navbar.php'; ?>
         <div class="page-header p-3 pb-1">
             <h1>Order Details</h1>
+            <?php
+            // Include database connection
+            include 'config/database.php';
+
+            // Get order ID from URL parameter
+            $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+
+            // Fetch customer name and order date from the database
+            $orderInfoQuery = "SELECT customers.first_name, order_summary.order_date FROM order_summary INNER JOIN customers ON order_summary.customer_id = customers.id WHERE order_summary.id = :id";
+            $orderInfoStmt = $con->prepare($orderInfoQuery);
+            $orderInfoStmt->bindParam(":id", $id);
+            $orderInfoStmt->execute();
+            $orderInfoRow = $orderInfoStmt->fetch(PDO::FETCH_ASSOC);
+
+            echo "<p><strong>Customer Name:</strong> {$orderInfoRow['first_name']}</p>";
+            echo "<p><strong>Order Date:</strong> {$orderInfoRow['order_date']}</p>";
+            ?>
         </div>
         <?php
-        $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
-        include 'config/database.php';
+        // Fetch order details from the database
         $query = "SELECT order_details.id, products.name, order_details.quantity, products.price, products.promotion_price FROM order_details INNER JOIN products ON order_details.product_id = products.id WHERE order_details.order_id =:id";
         $stmt = $con->prepare($query);
         $stmt->bindParam(":id", $id);
@@ -43,11 +61,9 @@
                 echo "<td>{$quantity}</td>";
                 echo "<td class='text-end'>";
                 if (!empty($promotion_price)) {
-                    // Display promotion price if available
                     echo "<div class='text-decoration-line-through'>" . number_format($price, 2) . "</div>";
                     echo number_format($promotion_price, 2);
                 } else {
-                    // Display regular price
                     echo number_format($price, 2);
                 }
                 echo "</td>";
@@ -57,15 +73,15 @@
             }
             echo "<tr>";
             echo "<td colspan='4' class='text-end'><strong>Total:</strong></td>";
-            echo "<td class='text-end'>" . number_format($totalPrice, 2) . "</td>"; // 添加 number_format 格式化
+            echo "<td class='text-end'>" . number_format($totalPrice, 2) . "</td>";
             echo "</tr>";
             echo "</table>";
             echo "<a href='order_read.php' class='btn btn-danger'>Back to order list</a>";
             echo "</div>";
         } else {
             echo '<div class="p-3">
-                <div class="alert alert-danger">No records found.</div>
-            </div>';
+                    <div class="alert alert-danger">No records found.</div>
+                </div>';
         }
         ?>
     </div>
